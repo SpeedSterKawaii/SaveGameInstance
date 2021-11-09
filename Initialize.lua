34d5e799017b7f5ddf682486f9bc8402de3ef6b4
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-11-09 09:55:38 -0500

    Create Initialize.lua

---
+++ Initialize.lua
@@ -0,0 +1,8 @@
+--[[
+Thank you github for hosting this.
+Thank you ROBLOX Studio for lua editor.
+Thank you ROBLOX core for anti-lag.
+Thank you google for icons/images.
+Thank you AtKn for function checker.
+Thank you Zer0 for lua script help.
+]]
