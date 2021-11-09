5fcd62ccb7100fce4e918d259c2b7b8ccf7c2ec0
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-11-09 10:13:38 -0500

    Update Initialize.lua

--- Initialize.lua
+++ Initialize.lua
@@ -5,4 +5,38 @@ Thank you ROBLOX core for anti-lag.
 Thank you google for icons/images.
 Thank you AtKn for function checker.
 Thank you Zer0 for lua script help.
+Thank you Windower for lua script help.
+Thank you Kampfkarren for some help.
+Thank you evaera for some help.
+Thank you validark for some help.
 ]]
+
+-- TRUE = yes, FALSE = no
+local SaveExplorer = true -- Set to TRUE to save EVERYTHING
+local SaveMapOnly = false 
+local SaveScripts = false
+local SaveLighting = false
+
+-- DONT set ANYTHING to FALSE!
+local Messages = true
+local Warnings = true
+local Printing = true
+local ErrorDump = true
+
+-- REPLACE with YOUR MESSAGES
+local LoadMessage = "Thanks to SpeedSterKawaii for this SaveInstance Script!"
+local ErrorMessage = "Oops! A script error has occurred and can't proceed!"
+local DoneMessage = "Successfully saved game! Credits to SpeedSterKawaii."
+
+-- PUBLIC FUNCTIONS (DO NOT MODIFY)
+local function PrintOutMessage(msg)
+  msg = LoadMessage
+  if (msg == "") then
+    warn("Error occurred, no message inserted inside 'LoadMessage'!")
+    else
+    print(LoadMessage)
+    wait(1)
+    msg = ""
+    LoadMessage = ""
+   end
+end
